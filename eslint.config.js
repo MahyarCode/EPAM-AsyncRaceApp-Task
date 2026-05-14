627cf61e8cab82e0fc757092002b7be10f6ff675
MahyarCode <mahyar.mousavi.1408@gmail.com>
2026-05-14 12:39:54 +0400

    style: configure eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,55 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import {
+  configs as airbnb,
+  plugins as airbnbPlugins,
+} from 'eslint-config-airbnb-extended';
 
 export default defineConfig([
   globalIgnores(['dist']),
+  airbnbPlugins.stylistic,
+  airbnbPlugins.importX,
+  airbnbPlugins.react,
+  airbnbPlugins.reactA11y,
+  airbnbPlugins.reactHooks,
+  airbnbPlugins.typescriptEslint,
+  ...airbnb.base.all,
+  ...airbnb.react.all,
+  {
+    rules: {
+      'import-x/no-extraneous-dependencies': [
+        'error',
+        {
+          devDependencies: [
+            '**/*.config.{js,ts,mjs,cjs}',
+            'eslint.config.js',
+            'vite.config.ts',
+          ],
+        },
+      ],
+    },
+  },
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    extends: [reactRefresh.configs.vite],
     languageOptions: {
       globals: globals.browser,
     },
+    rules: {
+      'max-lines-per-function': [
+        'error',
+        { max: 40, skipBlankLines: true, skipComments: true },
+      ],
+      'no-magic-numbers': [
+        'error',
+        {
+          ignore: [-1, 0, 1, 2],
+          ignoreArrayIndexes: true,
+          ignoreDefaultValues: true,
+          enforceConst: true,
+        },
+      ],
+    },
   },
-])
+]);
